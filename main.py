cc55a1e4c546944b1cfcad50f99e7bd5ec45d024
Skylar Onstot <skylar@themousepotato.net>
2020-10-12 16:48:34 -0700

    Interface V1

---
+++ main.py
@@ -0,0 +1,53 @@
+class IAmongUsML:
+	"""
+	Deterministic methods
+	"""
+	def get_game_settings():
+		pass
+
+	def get_emergency_caller():
+		pass
+
+	def vote():
+		pass
+
+	def get_eliminated_players():
+		pass
+
+	
+
+	"""
+	Binary models
+	"""
+	def kill():
+		pass
+
+	def report():
+		pass
+
+	def move_left():
+		pass
+
+	def move_right():
+		pass
+
+	def move_up():
+		pass
+
+	def move_down():
+		pass
+
+	def call_meeting():
+		pass
+
+	def vent():
+		pass
+
+	def fake_task():
+		pass
+
+	"""
+	Numerical models
+	"""
+	def ():
+		pass
